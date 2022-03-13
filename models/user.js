4a91972f11f00e3eaa8fe87858d2d63e2d782dad
Shy <fleetwoodshybreasia@gmail.com>
2022-03-13 13:59:17 -0400

    updated user.js

--- models/user.js
+++ models/user.js
@@ -13,18 +13,37 @@ const UserSchema = new Schema (
             type: String,
             unique: true,
             required: 'Must use valid e-mail',
-            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
-        }
+            match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/, 'Please enter a valid e-mail address']
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-)
-thoughts
+);
 
-Array of _id values referencing the Thought model
-
-friends
-
-Array of _id values referencing the User model (self-reference)
 
 const User = model('User', UserSchema);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 
 model.exports = User;
